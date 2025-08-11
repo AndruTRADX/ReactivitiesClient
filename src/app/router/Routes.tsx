c13e81e5faad198721df5ab47de903226c8865b2
Andres Rodriguez <andrudeluxe@gmail.com>
2025-08-11 15:48:53 -0500

    mobx

--- src/app/router/Routes.tsx
+++ src/app/router/Routes.tsx
@@ -1,38 +1,43 @@
-import { createBrowserRouter } from "react-router";
-import App from "../layout/App";
-import HomePage from "../../features/home/HomePage";
-import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import ActivityForm from "../../features/activities/form/ActivityForm";
-import ActivityDetailsPage from "../../features/activities/details/ActivityDetailsPage";
+import { createBrowserRouter } from 'react-router'
+import App from '../layout/App'
+import HomePage from '../../features/home/HomePage'
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard'
+import ActivityForm from '../../features/activities/form/ActivityForm'
+import ActivityDetailsPage from '../../features/activities/details/ActivityDetailsPage'
+import Counter from '../../features/counter/Counter'
 
 export const router = createBrowserRouter([
   // This works as a layout from Next.js OMG so useful
   // Everything within <App /> acts as a Layout component
   // The children will be whats within the <App /> (Layout)
   {
-    path: "/",
+    path: '/',
     element: <App />,
     children: [
       {
-        path: "",
+        path: '',
         element: <HomePage />,
       },
       {
-        path: "activities",
+        path: 'activities',
         element: <ActivityDashboard />,
       },
       {
-        path: "activities/:id",
+        path: 'activities/:id',
         element: <ActivityDetailsPage />,
       },
       {
-        path: "createActivity",
+        path: 'createActivity',
         element: <ActivityForm key='create' />,
       },
       {
-        path: "editActivity/:id",
+        path: 'editActivity/:id',
         element: <ActivityForm />,
+      },
+      {
+        path: 'counter',
+        element: <Counter />,
       },
     ],
   },
-]);
+])
--- src/lib/hooks/useActivities.tsx
+++ src/lib/hooks/useActivities.tsx
@@ -1,63 +1,67 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import agent from "../api/agent";
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import agent from '../api/agent'
+import { useLocation } from 'react-router'
 
 export const useActivities = (id?: string) => {
-  const queryClient = useQueryClient();
+  const queryClient = useQueryClient()
+  const location = useLocation()
 
   const { data: activities, isPending } = useQuery({
-    queryKey: ["activities"],
+    queryKey: ['activities'],
     queryFn: async () => {
-      const response = await agent.get<Activity[]>("/activities");
-      return response.data;
-    },
-  });
+      const response = await agent.get<Activity[]>('/activities')
+      return response.data
+    }, 
+    enabled: !id && location.pathname == '/activities',
+    // staleTime: 1000 * 60 * 5 /* Specifies how much we can wait to fetch the data again */
+  })
 
   const { data: activity, isLoading: isLoadingActivity } = useQuery({
-    queryKey: ["activities", id],
+    queryKey: ['activities', id],
     queryFn: async () => {
-      const response = await agent.get<Activity>(`/activities/${id}`);
-      return response.data;
+      const response = await agent.get<Activity>(`/activities/${id}`)
+      return response.data
     },
     // Here we specify that we will only fetch the data if the id exists
     enabled: !!id,
-  });
+  })
 
   const createActivity = useMutation({
     mutationFn: async (activity: Activity) => {
-      const response = await agent.post("/activities", activity);
-      return response.data;
+      const response = await agent.post('/activities', activity)
+      return response.data
     },
     onSuccess: async () => {
       // onSuccess, we update the cached activities, to get the latest ones
       await queryClient.invalidateQueries({
-        queryKey: ["activities"],
-      });
+        queryKey: ['activities'],
+      })
     },
-  });
+  })
 
   const updateActivity = useMutation({
     mutationFn: async (activity: Activity) => {
-      await agent.put("/activities", activity);
+      await agent.put('/activities', activity)
     },
     onSuccess: async () => {
       // onSuccess, we update the cached activities, to get the latest ones
       await queryClient.invalidateQueries({
-        queryKey: ["activities"],
-      });
+        queryKey: ['activities'],
+      })
     },
-  });
+  })
 
   const deleteActivity = useMutation({
     mutationFn: async (id: string) => {
-      await agent.delete(`/activities/${id}`);
+      await agent.delete(`/activities/${id}`)
     },
     onSuccess: async () => {
       // onSuccess, we update the cached activities, to get the latest ones
       await queryClient.invalidateQueries({
-        queryKey: ["activities"],
-      });
+        queryKey: ['activities'],
+      })
     },
-  });
+  })
 
   return {
     activities,
@@ -67,5 +71,5 @@ export const useActivities = (id?: string) => {
     updateActivity,
     createActivity,
     deleteActivity,
-  };
-};
+  }
+}
